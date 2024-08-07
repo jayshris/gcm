2e95f1619bf5fd99feb3af9b33558eefc77ad0a1
jayshris <jayshri@gmail.com>
2024-08-07 17:43:16 +0530

    changes

--- app/Controllers/LoadingReceipt.php
+++ app/Controllers/LoadingReceipt.php
@@ -204,7 +204,12 @@ class LoadingReceipt extends BaseController
     $this->view['states'] = $stateModel->where(['isStatus' => '1'])->orderBy('state_name', 'ASC')->findAll();
     $this->view['offices'] = $this->OModel->where('status', '1')->findAll();
     $this->view['bookings'] = $this->BookingsModel->where('approved', '1')->findAll();
-    $this->view['vehicles'] = $this->VModel->where('status', 1)->findAll();
+    // $this->view['vehicles'] = $this->VModel->where('status', 1)->findAll();
+    $this->view['vehicles'] =  $this->BookingsModel->select('v.id,v.rc_number') 
+    ->join('vehicle v','bookings.vehicle_id = v.id')->orderBy('v.id', 'desc')
+    ->where('bookings.approved', '1')
+    ->groupBy('bookings.vehicle_id')
+    ->findAll();
    
     //consignor, consignees changes
       
