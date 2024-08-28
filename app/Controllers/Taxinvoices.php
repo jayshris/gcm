99ca3b6f3992585a0fb43b241361f02ff6d86017
jayshris <jayshri@gmail.com>
2024-08-28 16:46:55 +0530

    tax invoice

---
+++ app/Controllers/Taxinvoices.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Controllers;
+   
+use App\Models\ProfileModel;
+use App\Models\BookingsModel;
+use App\Models\CustomersModel;
+use App\Models\TaxInvoiceModel;
+use App\Models\ExpenseHeadModel;
+use App\Controllers\BaseController;
+use App\Models\BookingExpensesModel;
+use App\Models\ProformaInvoiceModel; 
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Taxinvoices extends BaseController
+{ 
+    public $session; 
+    public $BookingsModel;
+    public $TaxInvoiceModel;
+    public $BEModel;
+    public $ExpenseHeadModel;
+    public $CModel;
+    public $added_by;
+    public $profile;
+    public function __construct()
+    { 
+      $this->session = \Config\Services::session(); 
+      $this->BookingsModel = new BookingsModel();
+      $this->TaxInvoiceModel= new TaxInvoiceModel();
+      $this->BEModel = new BookingExpensesModel();
+      $this->ExpenseHeadModel = new ExpenseHeadModel();    
+      $this->CModel = new CustomersModel();
+      $this->added_by = isset($_SESSION['id']) ? $_SESSION['id'] : '0';
+      $this->profile = new ProfileModel();
+    }
+  
+    public function index()
+    {    
+        $this->TaxInvoiceModel->select('*');  
+        $this->view['invoices']  = $this->TaxInvoiceModel->select('tax_invoices.*,b.booking_number')
+        ->join('bookings b','b.id=tax_invoices.booking_id')->findAll();
+        return view('TaxInvoices/index', $this->view); 
+    } 
+    function getvehicles(){
+        return  $this->BookingsModel->select('v.id,v.rc_number') 
+        ->join('vehicle v','bookings.vehicle_id = v.id') 
+        ->where(['bookings.status >='=> '11']) 
+        ->orderBy('v.id', 'desc')
+        ->groupBy('bookings.vehicle_id')
+        ->findAll();
+    }
+    function getBooking(){
+      return $this->BookingsModel->where(['status >='=> '11'])->findAll();  
+    }
+    function create(){     
+      $this->view['bookings'] = $this->getBooking();  
+      $this->view['vehicles'] =  $this->getvehicles();
+
+      if($this->request->getPost()){
+        $error = $this->validate([ 
+          'booking_id'   =>  'required',
+          'bill_to_party_id'   =>  'required',
+          'total_freight'   =>  'required'
+        ]);   
+        if (!$error) {
+          $this->view['error']   = $this->validator;
+        } else {  
+          $this->update_tax_invoice($this->request->getVar());
+           
+          $this->session->setFlashdata('success', 'Tax Invoice Added Successfully');
+          return $this->response->redirect(base_url('/taxinvoices'));
+        }
+      }
+      return view('TaxInvoices/form', $this->view); 
+    }
+  
+    function getVehicleBookings(){ 
+      $bookings = $this->BookingsModel->where('vehicle_id', $this->request->getPost('vehicle_id'))->where(['status >='=> '6'])->findAll();       
+      echo json_encode($bookings);exit;
+    }
+
+    function update_tax_invoice($post,$id =0){
+      // echo $id.'<pre>';print_r($post); 
+      if($id>0){
+        $data['id'] = $id;
+        $data['updated_by'] = $this->added_by;
+      }else{
+        $profile =  $this->profile->where('logged_in_userid',  session()->get('id'))->first();//echo __LINE__.'<pre>';print_r($profile);die;
+        $lastr = $this->TaxInvoiceModel->orderBy('id', 'desc')->first();
+        $lastr = isset($lastr['id']) ? ((int)$lastr['id']+1) : 1; 
+        $data['invoices_no'] = isset($profile['tax_invoice_prefix']) && !empty($profile['tax_invoice_prefix']) ? $profile['tax_invoice_prefix'].'/'.date('m').'/000'.$lastr : 'PR/'.date('m').'/000'.$lastr;
+        $data['created_by'] =$this->added_by;
+      }
+      $data['booking_id'] = $post['booking_id'];
+      $data['bill_to_party_id'] = $post['bill_to_party_id'];
+      $data['total_freight'] = $post['total_freight']; 
+      // echo  'Invoice <pre>';print_r($data);exit;
+      $this->TaxInvoiceModel->save($data);
+      
+      if($post['expense']){
+        // update Expences 
+        $this->BEModel->where('booking_id', $data['booking_id'])->delete();
+        // save expenses 
+        foreach ($post['expense'] as $key => $val) {
+          if(($post['expense'][$key]) || $post['expense_value'][$key] || isset($post['expense_flag_' . $key +1])){
+            $expense_data = [
+                'booking_id' => $data['booking_id'],
+                'expense' => $post['expense'][$key],
+                'value' => $post['expense_value'][$key],
+                'bill_to_party' =>isset($post['expense_flag_' . $key +1]) && ($post['expense_flag_' . $key +1] == 'on') ? '1' : '0'
+            ]; 
+            // echo  'expense_data <pre>';print_r($expense_data);
+            $this->BEModel->insert($expense_data);
+          }
+        }    
+        $booking['discount'] = $post['discount'];
+        $booking['balance'] = $post['balance'];
+        $booking['guranteed_wt'] = $post['guranteed_wt']; 
+        $booking['advance'] = $post['advance']; 
+        $booking['rate'] = $post['rate'];   
+      }
+      $booking['freight'] = $post['total_freight'];
+      // echo  $post['booking_id'].' booking <pre>';print_r($booking);exit;
+      $this->BookingsModel->update($post['booking_id'],$booking); 
+    } 
+
+    function edit($id){  
+      $this->view['bookings'] = $this->getBooking();  
+      $this->view['vehicles'] =  $this->getvehicles();
+      $this->view['token'] = $id; 
+      $this->view['invoice'] = $this->TaxInvoiceModel->where(['id' => $id])->first(); 
+      
+      if($this->request->getPost()){
+        $error = $this->validate([ 
+          'booking_id'   =>  'required',
+          'bill_to_party_id'   =>  'required',
+          'total_freight'   =>  'required'
+        ]);   
+        if (!$error) {
+          $this->view['error']   = $this->validator;
+        } else {            
+          $this->update_tax_invoice($this->request->getVar(),$id);           
+          $this->session->setFlashdata('success', 'Tax Invoice Updated Successfully');
+          return $this->response->redirect(base_url('/taxinvoices'));
+        }
+      }
+      return view('TaxInvoices/form', $this->view); 
+    } 
+  
+    public function delete($id = null)
+    {   
+        $this->TaxInvoiceModel->where('id', $id)->delete($id); 
+        $this->session->setFlashdata('success', 'Tax Invoice has been deleted successfully');
+        return $this->response->redirect(base_url('/taxinvoices')); 
+    }
+  
+    function preview($id){ 
+      $this->view['invoice'] = $this->TaxInvoiceModel
+      ->select('tax_invoices.*,b.*,p.*,v.rc_number,s.state_name,bps.state_name pickup_state,bds.state_name drop_state')
+      ->join('bookings b','b.id=tax_invoices.booking_id')
+      ->join('booking_pickups bp','b.id=bp.booking_id')
+      ->join('states bps', 'bp.state = bps.state_id','left')
+      ->join('booking_drops bd','b.id=bd.booking_id')
+      ->join('states bds', 'bd.state = bds.state_id','left')
+      ->join('vehicle v', 'v.id = b.vehicle_id','left')
+      ->join('customer c','c.id=tax_invoices.bill_to_party_id')
+      ->join('party p','c.party_id = p.id')
+      ->join('states s', 'p.state_id = s.state_id','left')
+      ->where(['tax_invoices.id' => $id])->first(); 
+
+      $this->view['booking_expences'] = $this->BEModel->select('booking_expenses.*,eh.*')
+      ->join('expense_heads eh','eh.id = booking_expenses.expense')
+      ->where('booking_expenses.booking_id', $this->view['invoice']['booking_id'])
+      ->where('booking_expenses.bill_to_party',1)
+      ->findAll();   
+      //  echo '<pre>';print_r($this->view['booking_expences']);exit;
+      return view('TaxInvoices/preview', $this->view); 
+    }
+
+    function getBookingExpense($id){ 
+      $this->view['booking_details'] = $this->BookingsModel->where(['id'=> $id])->first();  
+      $this->view['booking_expences'] = $this->BEModel->where('booking_id', $id)->findAll();  
+      $this->view['expense_heads'] =  $this->ExpenseHeadModel->orderBy('head_name', 'asc')->findAll();
+
+      // echo 'expense_heads <pre>';print_r($this->view['expense_heads']);
+      // echo 'booking_expences <pre>';print_r($this->view['booking_expences']);
+      // exit;
+      echo view('TaxInvoices/expense_block', $this->view);
+    } 
+    function getBookingCustomers($id){ 
+      $Customers =  $this->BookingsModel->select('bookings.id, c.id c_id ') 
+      ->join('customer c','bookings.customer_id = c.id')->where(['bookings.id'=> $id])->findAll();  
+
+      $Customers2 =  $this->BookingsModel->select('bookings.id,c.id c_id') 
+      ->join('customer c','bookings.bill_to_party = c.id')->where(['bookings.id'=> $id])->findAll();  
+
+      $Customers3 =  $this->BookingsModel->select('bookings.id,lr.consignor_id c_id') 
+      ->join('loading_receipts lr','bookings.id = lr.booking_id')
+      ->join('customer c','lr.consignor_id = c.id','left')->where(['bookings.id'=> $id])->findAll();  
+
+      $Customers4 =  $this->BookingsModel->select('bookings.id,lr.consignee_id c_id') 
+      ->join('loading_receipts lr','bookings.id = lr.booking_id')
+      ->join('customer c','lr.consignee_id = c.id','left')->where(['bookings.id'=> $id])->findAll();  
+      
+      $all_customers = array_merge($Customers,$Customers2,$Customers3,$Customers4); 
+      $unique_customers =  array_unique(array_column($all_customers, 'c_id'));
+      // echo 'all_customers<pre>';print_r($unique_customers);  
+      $customers = [];
+      if(!empty($unique_customers)){
+        $customers = $this->CModel->whereIN('customer.id',$unique_customers)->join('party', 'customer.party_id = party.id')->orderBy('party.party_name')->findAll();
+      }
+      echo json_encode($customers);exit;
+    }
+
+    function getBookingDetails($id){
+      $bookings = $this->BookingsModel->where('id', $id)->first();       
+      echo json_encode($bookings);exit;
+    }
+}
---
+++ app/Views/TaxInvoices/form.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?= $this->include('partials/title-meta') ?>
+	<?= $this->include('partials/head-css') ?>
+</head>
+<body>
+	<!-- Main Wrapper -->
+	<div class="main-wrapper">
+		<?= $this->include('partials/menu') ?>
+		<!-- Page Wrapper -->
+		<div class="page-wrapper">
+			<div class="content">
+				<div class="row">
+					<div class="col-md-12"> 
+						<?php $validation = \Config\Services::validation(); ?>
+						<?php
+						$session = \Config\Services::session();
+						if ($session->getFlashdata('success')) {
+							echo '
+								<div class="alert alert-success">' . $session->getFlashdata("success") . '</div>
+								';
+						}
+						?>
+						<div class="row">
+							<div class="col-xl-12 col-lg-12">
+								<!-- Settings Info -->
+								<div class="card">
+									<div class="card-body">
+										<div class="settings-form"> 
+											<?php echo form_open_multipart(base_url().$currentController.'/'.$currentMethod.((isset($token) && $token >0) ? '/'.$token : ''), ['name'=>'actionForm', 'id'=>'actionForm']);?>
+											<input type="hidden" id="id" value="<?= $token ?>" />
+											<div class="settings-sub-header">
+												<h4>Tax Invoice</h4>
+											</div>
+											<div class="profile-details">
+												<div class="row g-3">
+													<div class="col-md-4">
+														<label class="col-form-label">Vehicle Number</label> 
+														<select class="form-select select2" name="vehicle_id" id="vehicle_number" aria-label="Default select example" onchange="$.getVehicleBookings();">
+															<option value="">Select Vehicle</option>
+															<?php foreach ($vehicles as $o) { ?> 
+																<option value="<?= $o['id'] ?>"><?= $o['rc_number'] ?></option> 
+															<?php } ?>
+														</select>
+														<?php
+														if ($validation->getError('booking_date')) {
+															echo '<div class="alert alert-danger mt-2">' . $validation->getError('booking_date') . '</div>';
+														}
+														?>
+													</div>
+
+													<div class="col-md-4">
+														<label class="col-form-label">Booking Order No<span class="text-danger">*</span></label>
+														<select class="form-select select2" required name="booking_id" id="booking_id" aria-label="Default select example"  onchange="$.getBookingDetails();">
+														 	
+														<?php if($bookings){ foreach ($bookings as $o) { ?>
+															<option value="<?= $o['id'] ?>" <?= (isset($invoice['booking_id']) && ($invoice['booking_id'] == $o['id'])) ? 'selected' : ''?> ><?= $o['booking_number'] ?></option> 
+															<?php }}else{ ?>
+															<option value="">Select Booking No.</option>
+															<?php } ?>
+														</select>
+														<?php
+														if ($validation->getError('booking_id')) {
+															echo '<div class="alert alert-danger mt-2">' . $validation->getError('booking_id') . '</div>';
+														}
+														?>
+													</div>
+
+													<div class="col-md-4">
+														<label class="col-form-label">Customer Name<span class="text-danger">*</span></label>
+														<select class="form-select select2" required name="bill_to_party_id" id="bill_to_party_id" aria-label="Default select example">
+															<option value="">Select Customer</option> 
+														</select>
+														<input type="hidden" id="selecected_bill_to_party_id" value="<?= isset($invoice['bill_to_party_id']) && ($invoice['bill_to_party_id'] > 0) ? $invoice['bill_to_party_id'] : 0 ?>"/>
+														<?php
+														if ($validation->getError('bill_to_party_id')) {
+															echo '<div class="alert alert-danger mt-2">' . $validation->getError('bill_to_party_id') . '</div>';
+														}
+														?>
+													</div>	  
+
+													<div class="col-md-12"  id="expense_div_body"></div>
+												</div>
+												<br>
+											</div> 
+											<div class="submit-button">
+												<input type="submit" class="btn btn-primary" value="Save">
+												<a href="<?php echo base_url().$currentController; ?>"  class="btn btn-warning">Reset</a>
+												<a href="<?php echo base_url().$currentController; ?>" class="btn btn-light">Cancel</a>
+											</div>   
+										</div>
+									</div>
+								</div>
+								<!-- /Settings Info -->
+
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /Page Wrapper -->
+
+	</div>
+	<!-- /Main Wrapper -->
+
+	<?= $this->include('partials/vendor-scripts') ?>
+														
+	<script>
+	$(document).ready(function() {
+		$.getBookingDetails();
+    });
+	$.getVehicleBookings = function() {
+		var vehicle_id = $('#vehicle_number').val();
+		
+		$.ajax({
+			method: "POST",
+			url: '<?php echo base_url('taxinvoices/getVehicleBookings'); ?>' ,
+			data: {
+				vehicle_id: vehicle_id
+			},
+			dataType: "json",
+			success: function(res) { 
+				console.log(res);
+				var html  ='';
+				if(res){
+					$.each(res, function(i, item) {  
+						var selected = (i=0) ? 'selected' : '';
+						html += '<option value="'+item.id+'" selected="'+selected+'">'+item.booking_number+'</option>'; 
+					}); 
+					$('#booking_id').html(html); 
+					$('#booking_id').val($('#booking_id').val()).attr("selected","selected").trigger('change');  
+				} 
+				 
+			}
+		});
+	}
+
+	$.getBookingDetails = function() { 
+		var booking_id = $('#booking_id').val(); 
+		if(booking_id){
+			$.ajax({
+				method: "POST",
+				url: '<?php echo base_url('taxinvoices/getBookingExpense/'); ?>'+booking_id, 
+				success: function(res) { 
+					$('#expense_div_body').html(res);
+				}
+			});
+
+			$.ajax({
+				method: "POST",
+				url: '<?php echo base_url('taxinvoices/getBookingCustomers/'); ?>'+booking_id, 
+				dataType:'json',
+				success: function(res) { 
+					var html = '<option value="">Select Customer</option>';
+					if(res){
+						var selecected_bill_to_party_id = $('#selecected_bill_to_party_id').val();
+						$.each(res, function(i, val) {
+							var selected = (selecected_bill_to_party_id > 0) && (selecected_bill_to_party_id == val.id) ? 'selected' : '';
+							html += '<option value="'+val.id+'" '+selected+'>'+val.party_name+'</option>';
+						});
+					}
+					$('#bill_to_party_id').html(html);
+				}
+			});
+
+			$.ajax({
+				method: "POST",
+				url: '<?php echo base_url('taxinvoices/getBookingDetails/'); ?>'+booking_id, 
+				dataType:'json',
+				success: function(res) {  
+					if(res){
+						$('#total_freight').val(res.freight);
+					} 
+				}
+			});
+		} 			
+	}	
+
+	$.billToParty = function(index) {
+      if ($("#expense_flag_" + index).prop("checked")) {
+        $('#expense_' + index).addClass('bill');
+        $('#expense_' + index).removeClass('not_to_bill');
+      } else {
+        $('#expense_' + index).addClass('not_to_bill');
+        $('#expense_' + index).removeClass('bill');
+      }
+
+      $.calculation();
+    }
+
+    $.calculation = function() {
+      var rate_type = parseInt($('#rate_type').val());
+      var rate = parseFloat($('#rate').val());
+      var freight = 0; 
+
+      var notbilltotal = 0;
+        $('.not_to_bill').each(function() {
+          notbilltotal += parseFloat($(this).val());
+        });
+        $('#discount').val(notbilltotal.toFixed(2)); 
+      if (rate > 0) {
+
+        var billtotal = 0;
+        $('.bill').each(function() {
+          billtotal += parseFloat($(this).val());
+        });
+
+        console.log(billtotal);
+
+        freight = freight;
+
+        // for guranteed weight
+        if (rate_type == 1) {
+          var guranteed_wt = parseFloat($('#guranteed_wt').val());
+          freight = (rate * guranteed_wt) + billtotal;
+        } else {
+          freight = rate + billtotal;
+        }
+
+        $('#freight').val(freight.toFixed(2));
+
+        var advance = ($('#advance').val());
+        var discount = ($('#discount').val());
+        var balance = (freight - advance - discount).toFixed(2); 
+		// alert(freight + advance +discount + '= '+balance);
+        $('#balance').val(balance); 
+      }
+    }
+
+	$.delete = function(index, str) {
+      $('#del_' + str + '_' + index).remove();
+      $.calculation();
+    }
+
+	$.addExpense = function() {
+      var tot = $('#expense_table').children('tbody').children('tr').length;
+
+      console.log(tot);
+      $.ajax({
+        type: "POST",
+        url: "<?php echo base_url('booking/addExpense'); ?>",
+        data: {
+          index: tot
+        },
+        success: function(data) {
+          $('#expense_body').append(data);
+        }
+      })
+    }
+
+	</script>
+</body>
+
+</html>
